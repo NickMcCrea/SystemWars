e164246055c94446316d6ce88cb2498b26225c25
Creadude <nickmccrea@gmail.com>
2015-02-18 11:47:07 +0000

    Implementation of proper atmospheric scattering for space-based view of atmosphere

--- MonoGameOpenGL/MonoGameDirectX11/Screens/AtmosphereTest.cs
+++ MonoGameOpenGL/MonoGameDirectX11/Screens/AtmosphereTest.cs
@@ -20,6 +20,27 @@ namespace MonoGameDirectX11.Screens
     {
 
 
+
+        float m_Kr = 0.0025f;		// Rayleigh scattering constant
+        private float m_Kr4PI;
+        float m_Km = 0.0010f;	// Mie scattering constant
+        private float m_Km4PI;
+        float m_ESun = 20.0f;		// Sun brightness constant
+        float m_g = -0.990f;		// The Mie phase asymmetry factor
+        private float m_fExposure = 2.0f;
+
+        private float m_fInnerRadius = 6000;
+        private float m_fOuterRadius = 6300;
+        private float m_fScale;
+
+        private Vector3 m_fWavelength;
+        private Vector3 m_fWavelength4;
+
+
+        private float m_fRayleighScaleDepth = 0.25f;
+        private float m_fMieScaleDepth = 0.1f;
+
+
         private Texture2D atmosphereTexture;
         private EffectRenderComponent atmosphereRenderComponent;
         private GameObject planetObject;
@@ -29,6 +50,18 @@ namespace MonoGameDirectX11.Screens
             : base()
         {
 
+            m_Kr4PI = m_Kr * 4.0f * MathHelper.Pi;
+            m_Km4PI = m_Km * 4.0f * MathHelper.Pi;
+            m_fScale = 1 / (m_fOuterRadius - m_fInnerRadius);
+
+            m_fWavelength.X = 0.650f;		// 650 nm for red
+            m_fWavelength.Y = 0.570f;		// 570 nm for green
+            m_fWavelength.Z = 0.475f;		// 475 nm for blue
+            m_fWavelength4.X = (float)Math.Pow(m_fWavelength.X, 4.0f);
+            m_fWavelength4.Y = (float)Math.Pow(m_fWavelength.Y, 4.0f);
+            m_fWavelength4.Z = (float)Math.Pow(m_fWavelength.Z, 4.0f);
+
+         
             ProceduralSphere planet = new ProceduralSphere(50, 50);
             ProceduralSphere atmosphere = new ProceduralSphere(100, 100);
 
@@ -37,15 +70,15 @@ namespace MonoGameDirectX11.Screens
 
             atmosphereTexture = SystemCore.ContentManager.Load<Texture2D>("Textures/AtmosphereGradient3");
 
-            planet.Scale(6000);
-            atmosphere.Scale(6300);
+            planet.Scale(m_fInnerRadius);
+            atmosphere.Scale(m_fOuterRadius);
 
             planetObject = GameObjectFactory.CreateRenderableGameObjectFromShape(planet,
                 EffectLoader.LoadEffect("flatshaded"));
 
 
             atmosphereObject = GameObjectFactory.CreateRenderableGameObjectFromShape(atmosphere,
-                EffectLoader.LoadEffect("atmosphere"));
+                EffectLoader.LoadEffect("SkyFromSpace"));
 
 
             SystemCore.GameObjectManager.AddAndInitialiseGameObject(atmosphereObject);
@@ -54,12 +87,35 @@ namespace MonoGameDirectX11.Screens
             atmosphereRenderComponent = atmosphereObject.GetComponent<EffectRenderComponent>();
 
 
-            atmosphereRenderComponent.effect.Parameters["gTex"].SetValue(atmosphereTexture);
+            atmosphereRenderComponent.effect.Parameters["v3InvWavelength"].SetValue(new Vector3(1 / m_fWavelength4.X,
+                1 / m_fWavelength4.Y, 1 / m_fWavelength4.Z));
 
-            bool offCenter = true;
+            atmosphereRenderComponent.effect.Parameters["fInnerRadius"].SetValue(m_fInnerRadius);
+            atmosphereRenderComponent.effect.Parameters["fInnerRadius2"].SetValue(m_fInnerRadius * m_fInnerRadius);
+            atmosphereRenderComponent.effect.Parameters["fOuterRadius"].SetValue(m_fOuterRadius);
+            atmosphereRenderComponent.effect.Parameters["fOuterRadius2"].SetValue(m_fOuterRadius * m_fOuterRadius);
+
+            atmosphereRenderComponent.effect.Parameters["fKrESun"].SetValue(m_Kr * m_ESun);
+            atmosphereRenderComponent.effect.Parameters["fKmESun"].SetValue(m_Km * m_ESun);
+            atmosphereRenderComponent.effect.Parameters["fKr4PI"].SetValue(m_Kr4PI);
+            atmosphereRenderComponent.effect.Parameters["fKm4PI"].SetValue(m_Km4PI);
+
+            atmosphereRenderComponent.effect.Parameters["g"].SetValue(m_g);
+            atmosphereRenderComponent.effect.Parameters["g2"].SetValue(m_g * m_g);
+
+            atmosphereRenderComponent.effect.Parameters["fScale"].SetValue(1.0f / (m_fOuterRadius - m_fInnerRadius));
+            atmosphereRenderComponent.effect.Parameters["fScaleDepth"].SetValue(m_fRayleighScaleDepth);
+            atmosphereRenderComponent.effect.Parameters["fScaleOverScaleDepth"].SetValue(1.0f/
+                                                                                         (m_fOuterRadius -
+                                                                                          m_fInnerRadius)/
+                                                                                         m_fRayleighScaleDepth);
+
+           
+
+
+            bool offCenter = false;
 
             if (offCenter)
-
             {
                 planetObject.Transform.Translate(new Vector3(10000, 0, 0));
                 atmosphereObject.Transform.Translate(new Vector3(10000, 0, 0));
@@ -68,30 +124,37 @@ namespace MonoGameDirectX11.Screens
 
             else
             {
-                mouseCamera.World.Translation = new Vector3(15000, 0, 0);
+                mouseCamera.World.Translation = new Vector3(0, 0, 0);
             }
 
         }
 
         public override void Update(GameTime gameTime)
         {
-         
+
             //planetObject.Transform.Translate(new Vector3(1,0,0));
             //atmosphereObject.Transform.Translate(new Vector3(1, 0, 0));
             base.Update(gameTime);
         }
 
-        private float angle;
-        Vector3 lightPos = new Vector3(1,0,0);
+     
         public override void Render(GameTime gameTime)
         {
-            //gLamp0DirPos
-            Vector3 lightAngle = Vector3.Transform(lightPos, Matrix.CreateRotationY(angle));
-            atmosphereRenderComponent.effect.Parameters["gLamp0DirPos"].SetValue(lightAngle);
-            angle += 0.01f;
-            atmosphereRenderComponent.effect.Parameters["CameraPositionInObjectSpace"].SetValue(mouseCamera.Position - atmosphereObject.Transform.WorldMatrix.Translation);
-            atmosphereRenderComponent.effect.Parameters["AtmosphereRadius"].SetValue(6300f);
-            atmosphereRenderComponent.effect.Parameters["SurfaceRadius"].SetValue(6000f);
+
+            Vector3 lightPos = Vector3.Zero;
+            foreach (SceneLight light in SystemCore.ActiveScene.LightsInScene)
+            {
+                if (light is DiffuseLight)
+                    lightPos = ((DiffuseLight) light).LightDirection;
+
+            }
+
+            float camHeight = (mouseCamera.Position - atmosphereObject.Transform.WorldMatrix.Translation).Length();
+            atmosphereRenderComponent.effect.Parameters["fCameraHeight"].SetValue(camHeight);
+            atmosphereRenderComponent.effect.Parameters["fCameraHeight2"].SetValue(camHeight*camHeight);
+            atmosphereRenderComponent.effect.Parameters["v3LightPos"].SetValue(lightPos);
+            atmosphereRenderComponent.effect.Parameters["v3CameraPos"].SetValue(mouseCamera.Position);
+
 
             DebugText.Write(SystemCore.ActiveCamera.Position.ToString());
 
---
+++ MonoGameOpenGL/MonoGameEngineContentProject/MonoGameEngineContentProject/MonoGameEngineContentProjectContent/Effects/SM5.0/SkyFromSpace.fx
@@ -0,0 +1,129 @@
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+
+
+uniform float3 v3CameraPos;		//HOOKED UP
+uniform float3 v3LightPos;		//HOOKED UP
+uniform float3 v3InvWavelength;	//HOOKED UP
+uniform float fCameraHeight;	 //HOOKED UP
+uniform float fCameraHeight2;	//
+uniform float fOuterRadius;		//
+uniform float fOuterRadius2;	//
+uniform float fInnerRadius;		//
+uniform float fInnerRadius2;	//
+uniform float fKrESun;			//
+uniform float fKmESun;			//
+uniform float fKr4PI;			//
+uniform float fKm4PI;			//
+uniform float fScale;			//
+uniform float fScaleDepth;		//
+uniform float fScaleOverScaleDepth;	//
+uniform float g;                 //
+uniform float g2;                //
+const int nSamples = 2;
+const float fSamples = 2.0;       //
+
+float4 v3Direction;
+
+struct VertexShaderInput
+{
+    float4 Position : SV_POSITION;
+
+    // TODO: add input channels such as texture
+    // coordinates and vertex colors here.
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : SV_POSITION;
+	float4 gl_FrontSecondaryColor : TEXCOORD0;
+	float4 gl_FrontColor : TEXCOORD1;
+	float3 v3Direction : TEXCOORD2;
+
+};
+
+float scale(float fCos)
+{
+	float x = 1.0 - fCos;
+	return fScaleDepth * exp(-0.00287 + x*(0.459 + x*(3.83 + x*(-6.80 + x*5.25))));
+}
+
+VertexShaderOutput VertexShaderFunction(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+
+	float4 worldPosition = mul(input.Position, World);
+    float4 viewPosition = mul(worldPosition, View);
+    output.Position = mul(viewPosition, Projection);
+
+	float3 v3Pos = input.Position.xyz;
+	float3 v3Ray = v3Pos - v3CameraPos;
+	float fFar = length(v3Ray);
+	v3Ray /= fFar;
+
+	// Calculate the closest intersection of the ray with the outer atmosphere (which is the near point of the ray passing through the atmosphere)
+	float B = 2.0 * dot(v3CameraPos, v3Ray);
+	float C = fCameraHeight2 - fOuterRadius2;
+	float fDet = max(0.0, B*B - 4.0 * C);
+	float fNear = 0.5 * (-B - sqrt(fDet));
+
+	// Calculate the ray's starting position, then calculate its scattering offset
+    float3 v3Start = v3CameraPos + v3Ray * fNear;
+	fFar -= fNear;
+	float fStartAngle = dot(v3Ray, v3Start) / fOuterRadius;
+	float fStartDepth = exp(-1.0 / fScaleDepth);
+	float fStartOffset = fStartDepth*scale(fStartAngle);
+
+    // Initialize the scattering loop variables
+	//gl_FrontColor = vec4(0.0, 0.0, 0.0, 0.0);
+	float fSampleLength = fFar / fSamples;
+	float fScaledLength = fSampleLength * fScale;
+	float3 v3SampleRay = v3Ray * fSampleLength;
+	float3 v3SamplePoint = v3Start + v3SampleRay * 0.5;
+
+	// Now loop through the sample rays
+	float3 v3FrontColor = float3(0.0, 0.0, 0.0);
+	for(int i=0; i<nSamples; i++)
+	{
+		float fHeight = length(v3SamplePoint);
+		float fDepth = exp(fScaleOverScaleDepth * (fInnerRadius - fHeight));
+		float fLightAngle = dot(v3LightPos, v3SamplePoint) / fHeight;
+		float fCameraAngle = dot(v3Ray, v3SamplePoint) / fHeight;
+		float fScatter = (fStartOffset + fDepth*(scale(fLightAngle) - scale(fCameraAngle)));
+		float3 v3Attenuate = exp(-fScatter * (v3InvWavelength * fKr4PI + fKm4PI));
+		v3FrontColor += v3Attenuate * (fDepth * fScaledLength);
+		v3SamplePoint += v3SampleRay;
+	}
+
+	// Finally, scale the Mie and Rayleigh colors and set up the varying variables for the pixel shader
+	output.gl_FrontSecondaryColor.rgb = v3FrontColor * fKmESun;
+	output.gl_FrontColor.rgb = v3FrontColor * (v3InvWavelength * fKrESun);
+	output.v3Direction = v3CameraPos - v3Pos;
+
+    return output;
+}
+
+float4 PixelShaderFunction(VertexShaderOutput input) : COLOR0
+{
+    float fCos = dot(v3LightPos,input.v3Direction) / length(input.v3Direction);
+	float fMiePhase = 1.5 * ((1.0 - g2) / (2.0 + g2)) * (1.0 + fCos*fCos) / pow(1.0 + g2 - 2.0*g*fCos, 1.5);
+    float4 gl_FragColor = input.gl_FrontColor + fMiePhase * input.gl_FrontSecondaryColor;
+	gl_FragColor.a = gl_FragColor.b;
+	return gl_FragColor;
+}
+
+technique Technique1
+{
+    pass Pass1
+    {
+	    ZEnable = false;
+		ZWriteEnable = false;
+		CullMode = CCW;
+		AlphaBlendEnable = true;
+		SrcBlend = One ;
+		DestBlend = InvSrcAlpha;
+        VertexShader = compile vs_5_0 VertexShaderFunction();
+        PixelShader = compile ps_5_0 PixelShaderFunction();
+    }
+}
