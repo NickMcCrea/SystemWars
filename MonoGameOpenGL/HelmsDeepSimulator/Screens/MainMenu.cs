dca4d7d6571697a2567812f27e29e96b99633162
Creadude <nickmccrea@gmail.com>
2015-06-02 16:53:26 +0100

    Adding new project

---
+++ MonoGameOpenGL/HelmsDeepSimulator/Screens/MainMenu.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGameEngineCore;
+using MonoGameEngineCore.GUI;
+using MonoGameEngineCore.GUI.Controls;
+
+namespace HelmsDeepSimulator.Screens
+{
+    public class MainMenuScreen : Screen
+    {
+        Label titleLabel;
+        Label startLabel;
+
+        public MainMenuScreen()
+            : base()
+        {
+
+
+            titleLabel = new Label(GUIFonts.Fonts["neuropolitical"], "Helms Deep Simulator");
+          
+            Vector2 titlePoint = GUIManager.ScreenMiddle();
+            titlePoint -= new Vector2(0, SystemCore.GraphicsDevice.Viewport.Height / 3);
+            titleLabel.SetPosition(titlePoint);
+            FadeTransition fadeIn = new FadeTransition(0, 1, 1000, 50000);
+            titleLabel.ApplyTransition(fadeIn);
+            SystemCore.GUIManager.AddControl(titleLabel);
+
+            startLabel = new Label(GUIFonts.Fonts["neuropolitical"], "Press Start");
+            Vector2 startLabelPoint = GUIManager.ScreenMiddle();
+            startLabel.SetPosition(startLabelPoint);
+            FadeTransition fadeInStart = new FadeTransition(0, 1, 4000, 50000);
+            startLabel.ApplyTransition(fadeInStart);
+            SystemCore.GUIManager.AddControl(startLabel);
+
+            GamePadButtonEvent gamePadButtonEvent = new GamePadButtonEvent(Buttons.Start);
+            input.AddBinding(new InputBinding("startScreenActivate", gamePadButtonEvent));
+            input.AddBinding(input.AddKeyPressBinding("startScreenActivate", Keys.Space));
+
+        }
+
+        public override void OnRemove()
+        {
+            SystemCore.GUIManager.ClearAllControls();
+
+            base.OnRemove();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (input.EvaluateInputBinding("startScreenActivate"))
+            {
+                SystemCore.ScreenManager.AddAndSetActive(new MainGameScreen());
+            }
+
+            base.Update(gameTime);
+        }
+
+        public override void Render(GameTime gameTime)
+        {
+            base.Render(gameTime);
+        }
+    }
+}
