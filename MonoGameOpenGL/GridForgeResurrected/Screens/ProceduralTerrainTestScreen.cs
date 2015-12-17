454d800916c774fdd35b85a05befa9840f94da77
Creadude <nickmccrea@gmail.com>
2015-12-17 17:43:51 +0000

    yada

--- MonoGameOpenGL/GridForgeResurrected/Screens/ProceduralTerrainTestScreen.cs
+++ MonoGameOpenGL/GridForgeResurrected/Screens/ProceduralTerrainTestScreen.cs
@@ -1,4 +1,5 @@
 ﻿using BEPUphysics.CollisionRuleManagement;
+using LibNoise.Modifiers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGameEngineCore;
@@ -13,7 +14,7 @@ using SystemWar;
 
 namespace GridForgeResurrected.Screens
 {
-   
+
 
     class ProceduralTerrainTestScreen : Screen
     {
@@ -22,8 +23,8 @@ namespace GridForgeResurrected.Screens
         Atmosphere atmosphere;
         private Vector3 startPos;
         private float planetSize = 500f;
-  
-      
+
+
         public ProceduralTerrainTestScreen()
         {
             startPos = new Vector3(0, planetSize * 1.25f, 0);
@@ -47,15 +48,20 @@ namespace GridForgeResurrected.Screens
             Effect effect = EffectLoader.LoadSM5Effect("AtmosphericScatteringGround");
 
 
+            var one = AddTerrainSegment(100, 0, 0);
+            var two = AddTerrainSegment(100, 99, 0);
+            two.Transform.Translate(new Vector3(99, 0, 0));
+
+            var three = AddTerrainSegment(100, -99, 0);
+            three.Transform.Translate(new Vector3(-99, 0, 0));
+
+
+
             ProceduralSphereTwo sphere = new ProceduralSphereTwo(100);
             sphere.SetColor(Color.DarkOrange);
             sphere.Scale(planetSize);
             var obj = GameObjectFactory.CreateRenderableGameObjectFromShape(sphere, effect);
-
-
-            
             SystemCore.GameObjectManager.AddAndInitialiseGameObject(obj);
-
             helper = new GroundScatteringHelper(effect, planetSize * 1.05f, planetSize);
             atmosphere = new Atmosphere(planetSize * 1.05f, planetSize);
             SystemCore.GameObjectManager.AddAndInitialiseGameObject(atmosphere);
@@ -63,23 +69,25 @@ namespace GridForgeResurrected.Screens
 
         private GameObject AddTerrainSegment(float size, float sampleX, float sampleY)
         {
-            
+
 
 
             int vertCount = 100;
             Heightmap heightmap = NoiseGenerator.CreateHeightMap(NoiseGenerator.RidgedMultiFractal(0.1f), vertCount,
-                size/vertCount, 10f, sampleX, sampleY,
+                size / vertCount, 10f, sampleX, sampleY,
                 1);
 
             GameObject terrain = new GameObject("terrain");
 
-            //we want to center it
+
+
+
             var verts =
                 BufferBuilder.VertexBufferBuild(heightmap.GenerateVertexArray());
 
 
             var indices = BufferBuilder.IndexBufferBuild(heightmap.GenerateIndices());
-            terrain.AddComponent(new RenderGeometryComponent(verts, indices, indices.IndexCount/3));
+            terrain.AddComponent(new RenderGeometryComponent(verts, indices, indices.IndexCount / 3));
             terrain.AddComponent(new EffectRenderComponent(EffectLoader.LoadSM5Effect("flatshaded")));
 
 
@@ -107,7 +115,7 @@ namespace GridForgeResurrected.Screens
 
         }
 
-     
+
         public override void Render(GameTime gameTime)
         {
             SystemCore.GraphicsDevice.Clear(Color.Gray);
