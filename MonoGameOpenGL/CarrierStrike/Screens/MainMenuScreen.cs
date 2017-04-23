732f80888b4c5081f627940655aef7782c9fc3ec
NickMcCrea <nickmccrea@gmail.com>
2017-04-23 11:39:32 +0100

    CarrierStrike Project added with engine content

---
+++ MonoGameOpenGL/CarrierStrike/Screens/MainMenuScreen.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using MonoGameDirectX11;
+using MonoGameEngineCore;
+using MonoGameEngineCore.GUI;
+using MonoGameEngineCore.GUI.Controls;
+using MonoGameEngineCore.ScreenManagement;
+
+namespace CarrierStrike
+{
+    class MainMenuScreen : Screen
+    {
+        public MainMenuScreen()
+            : base()
+        {
+
+            string screenOne = "One";
+            string screenTwo = "Two";
+            string screenThree = "Three";
+
+
+            SystemCore.GetSubsystem<GUIManager>().CreateDefaultMenuScreen("Main Menu", SystemCore.ActiveColorScheme, screenOne, screenTwo, screenThree);
+            SystemCore.CursorVisible = true;
+
+            Button b = SystemCore.GetSubsystem<GUIManager>().GetControl(screenOne) as Button;
+            b.OnClick += (sender, args) =>
+            {
+                SystemCore.ScreenManager.AddAndSetActive(new RenderTestScreen());
+            };
+
+            Button a = SystemCore.GetSubsystem<GUIManager>().GetControl(screenTwo) as Button;
+            a.OnClick += (sender, args) =>
+            {
+                SystemCore.ScreenManager.AddAndSetActive(new RenderTestScreen());
+            };
+
+            Button c = SystemCore.GetSubsystem<GUIManager>().GetControl(screenThree) as Button;
+            c.OnClick += (sender, args) =>
+            {
+                SystemCore.ScreenManager.AddAndSetActive(new RenderTestScreen());
+            };
+
+
+
+        }
+
+        public override void OnRemove()
+        {
+            SystemCore.GUIManager.ClearAllControls();
+
+            base.OnRemove();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Render(GameTime gameTime)
+        {
+            base.Render(gameTime);
+        }
+    }
+}
