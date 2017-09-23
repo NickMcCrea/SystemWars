03b07f30bd5c424a1cfe01c641a78dcc5ca8e837
NickMcCrea <nickmccrea@gmail.com>
2017-09-23 10:46:21 +0100

    Old game project

---
+++ MonoGameOpenGL/OldGameTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGameEngineCore;
+using MonoGameEngineCore.GUI;
+using MonoGameEngineCore.Rendering;
+
+namespace OldGameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemCore.ActiveColorScheme = ColorScheme.ColorSchemes["flatui"];
+            ScreenResolutionName resToUse = ScreenResolutionName.WXGA;
+
+            if (System.Environment.MachineName == "NICKMCCREA-PC")
+                resToUse = ScreenResolutionName.WUXGA;
+
+            using (var game = new MonoEngineGame(typeof(MainMenuScreen), resToUse, DepthFormat.Depth24, true, true))
+                game.Run();
+        }
+    }
+}
